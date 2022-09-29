14814e4bd228a37da9602b150019fe43c2a57d30
Chu <cvhainb@gmail.com>
2022-09-29 16:57:55 +0700

    updated

--- resources/js/components/themes/storefront/templates/layout/Footer.vue
+++ resources/js/components/themes/storefront/templates/layout/Footer.vue
@@ -1,19 +1,15 @@
 <template>
     <footer class="mt-5 pt-3">
         <block-element menu-key="footer-middle" :block-loading="1"></block-element>
-        <section class="container">
-            <div class="row mt-3">
-                <div class="col-12 col-lg-3 col-md-6">
-                    <subscribe-form></subscribe-form>
-                </div>
-            </div>
-        </section>
         <section class="py-4 border-top mt-5 bg-gray-200">
             <div class="container">
-                <div class="row">
-                    <div class="col-lg-6">&copy; {{ storeConfig.store_name }} {{ year }}. All rights reserved.</div>
-                    <div class="col-lg-6 text-lg-end">
+                <div class="row align-items-center">
+                    <div class="col-md-8 order-md-0 order-1">
+                        &copy; {{ storeConfig.store_name }} {{ year }}. All rights reserved.
                         <img src="/storage/credit-card-accept.png" alt="Credit Cards" width="197" height="20">
+                    </div>
+                    <div class="col-md-4 text-md-end order-md-1 order-0 mb-3 mb-md-0">
+                        <subscribe-form></subscribe-form>
                     </div>
                 </div>
             </div>
