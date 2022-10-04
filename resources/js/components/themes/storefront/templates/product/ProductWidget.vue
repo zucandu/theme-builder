1dad6c29b66701ccc144eb0c4ee3a4a8b397c9a1
Chu <cvhainb@gmail.com>
2022-10-04 21:52:04 +0700

    updated

--- resources/js/components/themes/storefront/templates/product/ProductWidget.vue
+++ resources/js/components/themes/storefront/templates/product/ProductWidget.vue
@@ -21,13 +21,13 @@
             </div>
             <template v-if="item.quantity > 0">
                 <div v-if="+item.has_attributes === 0" class="inner__cart d-flex w-100 bottom-0 mt-3">
-                    <input type="number" v-model="cartQty[item.id]" class="form-control form-control-sm border-end-0">
+                    <!-- <input type="number" v-model="cartQty[item.id]" class="form-control form-control-sm border-end-0">
                     <button class="btn btn-sm btn-success rounded-0 border-start-0 text-white px-3" @click.stop="addToCart(item, cartQty[item.id])" data-bs-toggle="modal" data-bs-target="#z-cart-modal">
                         <svg xmlns="http://www.w3.org/2000/svg" width="16" height="16" fill="currentColor" class="bi bi-cart-plus" viewBox="0 0 16 16">
                             <path d="M9 5.5a.5.5 0 0 0-1 0V7H6.5a.5.5 0 0 0 0 1H8v1.5a.5.5 0 0 0 1 0V8h1.5a.5.5 0 0 0 0-1H9V5.5z"/>
                             <path d="M.5 1a.5.5 0 0 0 0 1h1.11l.401 1.607 1.498 7.985A.5.5 0 0 0 4 12h1a2 2 0 1 0 0 4 2 2 0 0 0 0-4h7a2 2 0 1 0 0 4 2 2 0 0 0 0-4h1a.5.5 0 0 0 .491-.408l1.5-8A.5.5 0 0 0 14.5 3H2.89l-.405-1.621A.5.5 0 0 0 2 1H.5zm3.915 10L3.102 4h10.796l-1.313 7h-8.17zM6 14a1 1 0 1 1-2 0 1 1 0 0 1 2 0zm7 0a1 1 0 1 1-2 0 1 1 0 0 1 2 0z"/>
                         </svg>
-                    </button>
+                    </button> -->
                 </div>
                 <div v-else class="inner__cart d-flex w-100 bottom-0 mt-3">
                     <router-link class="btn btn-sm btn-success text-white w-100 d-block" :to="`/${translation(item, 'slug', $i18n.locale)}`">{{ $t('Choose options') }}</router-link>
