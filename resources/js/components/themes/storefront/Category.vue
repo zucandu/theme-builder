2033d966bd65b2f980116489b8ba7f29202f1ad4
Chu <cvhainb@gmail.com>
2022-12-07 13:57:20 +0700

    updated

--- resources/js/components/themes/storefront/Category.vue
+++ resources/js/components/themes/storefront/Category.vue
@@ -6,13 +6,20 @@
                 <svg xmlns="http://www.w3.org/2000/svg" width="16" height="16" fill="currentColor" class="bi bi-arrow-right-short mx-2" viewBox="0 0 16 16">
                     <path fill-rule="evenodd" d="M4 8a.5.5 0 0 1 .5-.5h5.793L8.146 5.354a.5.5 0 1 1 .708-.708l3 3a.5.5 0 0 1 0 .708l-3 3a.5.5 0 0 1-.708-.708L10.293 8.5H4.5A.5.5 0 0 1 4 8z"/>
                 </svg>
-                <template>{{ categoryTranslation.name }}</template>
+                {{ categoryTranslation.name }}
             </div>
             <h1>{{ categoryTranslation.name }}</h1>
             <div class="col-lg-8 col-12 text-gray-500">{{ categoryTranslation.description }}</div>
         </div>
         <div v-else class="col-12 text-center my-4">
-
+            <div class="pb-3">
+                <div class="rounded bg-secondary opacity-25 px-4 py-2 d-inline-block"></div>
+                <svg xmlns="http://www.w3.org/2000/svg" width="16" height="16" fill="currentColor" class="bi bi-arrow-right-short mx-2" viewBox="0 0 16 16">
+                    <path fill-rule="evenodd" d="M4 8a.5.5 0 0 1 .5-.5h5.793L8.146 5.354a.5.5 0 1 1 .708-.708l3 3a.5.5 0 0 1 0 .708l-3 3a.5.5 0 0 1-.708-.708L10.293 8.5H4.5A.5.5 0 0 1 4 8z"/>
+                </svg>
+                <div class="rounded bg-secondary opacity-25 px-4 py-2 d-inline-block"></div>
+            </div>
+            <div class="py-3 px-5 rounded bg-secondary opacity-25 d-inline-block col-lg-2 col-6"></div>
         </div>
         <div class="zuc-listing-sidebar col-lg-3">
             <section class="zuc-listing-sidebar__filter">
@@ -81,10 +88,9 @@
                         </template>
                         <template v-else>
                             <div class="inner__cart d-flex w-100 bottom-0 mt-3">
-                                <button @click.prevent="showModal = true, picked = { id: item.id, name: translation(item, 'name', $i18n.locale) }" class="btn btn-info btn-sm text-white w-100" type="button">
-                                    <svg xmlns="http://www.w3.org/2000/svg" width="16" height="16" fill="currentColor" class="bi bi-send-plus" viewBox="0 0 16 16">
-                                        <path d="M15.964.686a.5.5 0 0 0-.65-.65L.767 5.855a.75.75 0 0 0-.124 1.329l4.995 3.178 1.531 2.406a.5.5 0 0 0 .844-.536L6.637 10.07l7.494-7.494-1.895 4.738a.5.5 0 1 0 .928.372l2.8-7Zm-2.54 1.183L5.93 9.363 1.591 6.602l11.833-4.733Z"/>
-                                        <path d="M16 12.5a3.5 3.5 0 1 1-7 0 3.5 3.5 0 0 1 7 0Zm-3.5-2a.5.5 0 0 0-.5.5v1h-1a.5.5 0 0 0 0 1h1v1a.5.5 0 0 0 1 0v-1h1a.5.5 0 0 0 0-1h-1v-1a.5.5 0 0 0-.5-.5Z"/>
+                                <button @click.prevent="showModal = true, picked = { id: item.id, name: translation(item, 'name', $i18n.locale) }" class="btn btn-sm btn-link d-flex align-items-center text-dark p-0 text-decoration-none" type="button">
+                                    <svg xmlns="http://www.w3.org/2000/svg" width="16" height="16" fill="currentColor" class="bi bi-envelope me-2" viewBox="0 0 16 16">
+                                        <path d="M0 4a2 2 0 0 1 2-2h12a2 2 0 0 1 2 2v8a2 2 0 0 1-2 2H2a2 2 0 0 1-2-2V4Zm2-1a1 1 0 0 0-1 1v.217l7 4.2 7-4.2V4a1 1 0 0 0-1-1H2Zm13 2.383-4.708 2.825L15 11.105V5.383Zm-.034 6.876-5.64-3.471L8 9.583l-1.326-.795-5.64 3.47A1 1 0 0 0 2 13h12a1 1 0 0 0 .966-.741ZM1 11.105l4.708-2.897L1 5.383v5.722Z"/>
                                     </svg>
                                     {{ $t('Notify') }}
                                 </button>
--- resources/js/components/themes/storefront/Manufacturer.vue
+++ resources/js/components/themes/storefront/Manufacturer.vue
@@ -1,11 +1,32 @@
 <template>
     <div class="zuc-listing-container row g-3 justify-content-center">
-        <div class="zuc-listing-sidebar col-lg-2">
+        <div v-if="manufacturerTranslation" class="col-12 text-center my-4">
+            <div class="small text-gray-500 pb-3">
+                <router-link to="/">{{ $t('Home') }}</router-link>
+                <svg xmlns="http://www.w3.org/2000/svg" width="16" height="16" fill="currentColor" class="bi bi-arrow-right-short mx-2" viewBox="0 0 16 16">
+                    <path fill-rule="evenodd" d="M4 8a.5.5 0 0 1 .5-.5h5.793L8.146 5.354a.5.5 0 1 1 .708-.708l3 3a.5.5 0 0 1 0 .708l-3 3a.5.5 0 0 1-.708-.708L10.293 8.5H4.5A.5.5 0 0 1 4 8z"/>
+                </svg>
+                {{ manufacturerTranslation.name }}
+            </div>
+            <h1>{{ manufacturerTranslation.name }}</h1>
+            <div class="col-lg-8 col-12 text-gray-500">{{ manufacturerTranslation.description }}</div>
+        </div>
+        <div v-else class="col-12 text-center my-4">
+            <div class="pb-3">
+                <div class="rounded bg-secondary opacity-25 px-4 py-2 d-inline-block"></div>
+                <svg xmlns="http://www.w3.org/2000/svg" width="16" height="16" fill="currentColor" class="bi bi-arrow-right-short mx-2" viewBox="0 0 16 16">
+                    <path fill-rule="evenodd" d="M4 8a.5.5 0 0 1 .5-.5h5.793L8.146 5.354a.5.5 0 1 1 .708-.708l3 3a.5.5 0 0 1 0 .708l-3 3a.5.5 0 0 1-.708-.708L10.293 8.5H4.5A.5.5 0 0 1 4 8z"/>
+                </svg>
+                <div class="rounded bg-secondary opacity-25 px-4 py-2 d-inline-block"></div>
+            </div>
+            <div class="py-3 px-5 rounded bg-secondary opacity-25 d-inline-block col-lg-2 col-6"></div>
+        </div>
+        <div class="zuc-listing-sidebar col-lg-3">
             <section class="zuc-listing-sidebar__filter">
                 <listing-filters :filters="filters" :reset-filter="resetFilters" @updateContent="updateContent"></listing-filters>
             </section>
         </div>
-        <div class="zuc-listing-products col-lg-9 offset-lg-1">
+        <div class="zuc-listing-products col-lg-8 offset-lg-1">
             <section class="zuc-listing-products__sort d-sm-flex justify-content-between mb-5">
                 <div class="showing">{{ $t('Showing') }} {{ paginationShowing.from }} {{ $t('to') }} {{ paginationShowing.to }} {{ $t('of') }} {{ paginationShowing.total }} {{ $t('products') }}</div>
                 <div class="btn-group me-4">
@@ -22,14 +43,14 @@
                             <router-link :to="`/${translation(item, 'slug', $i18n.locale)}`" class="text-decoration-none">
                                 <img :src="`/storage/${storeConfig.medium_image_size}/${item.images[0].src}`" :width="storeConfig.medium_image_size" :height="storeConfig.medium_image_size" :alt="translation(item, 'name', $i18n.locale)" :id="`img-listing-${item.id}`" class="img-fluid">
                             </router-link>
-                        </div>
-                        <div class="inner__additional-images d-flex justify-content-center align-items-center">
-                            <template v-if="item.images.length > 1">
-                                <template v-for="(ai, index) in item.images" :key="ai">
-                                    <img v-if="index < 3" @mouseover="changeImgSrc(item.id, ai.src)" :src="`/storage/${storeConfig.small_image_size}/${ai.src}`" width="40" height="40" :alt="translation(item, 'name', $i18n.locale)" class="img-loading img-fluid cursor-pointer mx-1">
+                            <div class="inner__additional-images d-flex justify-content-center align-items-center py-2">
+                                <template v-if="item.images.length > 1">
+                                    <template v-for="(ai, index) in item.images" :key="ai">
+                                        <img v-if="index < 3" @mouseover="changeImgSrc(item.id, ai.src)" :src="`/storage/${storeConfig.small_image_size}/${ai.src}`" width="30" height="30" :alt="translation(item, 'name', $i18n.locale)" class="img-loading img-fluid cursor-pointer mx-1">
+                                    </template>
                                 </template>
-                            </template>
-                            <router-link v-if="item.images.length > 3" :to="`/${translation(item, 'slug', $i18n.locale)}`" class="square-4x small">+{{ item.images.length-3 }} {{ $t('options') }}</router-link>
+                                <router-link v-if="item.images.length > 3" :to="`/${translation(item, 'slug', $i18n.locale)}`" class="square-4x small text-gray-500 text-decoration-none">+{{ item.images.length-3 }} {{ $t('options') }}</router-link>
+                            </div>
                         </div>
                         <h3 class="inner__title h6 fw-light mb-0">
                             <router-link :to="`/${translation(item, 'slug', $i18n.locale)}`" class="text-decoration-none text-dark">
@@ -46,24 +67,29 @@
                         </div>
                         <template v-if="item.quantity > 0">
                             <div v-if="+item.has_attributes === 0" class="inner__cart d-flex w-100 bottom-0 mt-3">
-                                <input type="number" v-model="cartQty[item.id]" class="form-control form-control-sm border-end-0">
-                                <button class="btn btn-sm btn-success rounded-0 border-start-0 text-white px-3" @click.stop="addToCart(item, cartQty[item.id])" data-bs-toggle="modal" data-bs-target="#z-cart-modal">
-                                    <svg xmlns="http://www.w3.org/2000/svg" width="16" height="16" fill="currentColor" class="bi bi-cart-plus" viewBox="0 0 16 16">
+                                <button class="btn btn-sm btn-link d-flex align-items-center text-dark p-0 text-decoration-none" @click.stop="addToCart(item, cartQty[item.id])" data-bs-toggle="modal" data-bs-target="#z-cart-modal">
+                                    <svg xmlns="http://www.w3.org/2000/svg" width="16" height="16" fill="currentColor" class="bi bi-cart-plus me-2" viewBox="0 0 16 16">
                                         <path d="M9 5.5a.5.5 0 0 0-1 0V7H6.5a.5.5 0 0 0 0 1H8v1.5a.5.5 0 0 0 1 0V8h1.5a.5.5 0 0 0 0-1H9V5.5z"/>
                                         <path d="M.5 1a.5.5 0 0 0 0 1h1.11l.401 1.607 1.498 7.985A.5.5 0 0 0 4 12h1a2 2 0 1 0 0 4 2 2 0 0 0 0-4h7a2 2 0 1 0 0 4 2 2 0 0 0 0-4h1a.5.5 0 0 0 .491-.408l1.5-8A.5.5 0 0 0 14.5 3H2.89l-.405-1.621A.5.5 0 0 0 2 1H.5zm3.915 10L3.102 4h10.796l-1.313 7h-8.17zM6 14a1 1 0 1 1-2 0 1 1 0 0 1 2 0zm7 0a1 1 0 1 1-2 0 1 1 0 0 1 2 0z"/>
                                     </svg>
+                                    {{ $t('Add to cart') }}
                                 </button>
                             </div>
                             <div v-else class="inner__cart d-flex w-100 bottom-0 mt-3">
-                                <router-link class="btn btn-sm btn-success text-white w-100 d-block" :to="`/${translation(item, 'slug', $i18n.locale)}`">{{ $t('Choose options') }}</router-link>
+                                <router-link class="btn btn-sm btn-link d-flex align-items-center text-dark p-0 text-decoration-none" :to="`/${translation(item, 'slug', $i18n.locale)}`">
+                                    <svg xmlns="http://www.w3.org/2000/svg" width="16" height="16" fill="currentColor" class="bi bi-box-arrow-in-right me-2 opacity-50" viewBox="0 0 16 16">
+                                        <path fill-rule="evenodd" d="M6 3.5a.5.5 0 0 1 .5-.5h8a.5.5 0 0 1 .5.5v9a.5.5 0 0 1-.5.5h-8a.5.5 0 0 1-.5-.5v-2a.5.5 0 0 0-1 0v2A1.5 1.5 0 0 0 6.5 14h8a1.5 1.5 0 0 0 1.5-1.5v-9A1.5 1.5 0 0 0 14.5 2h-8A1.5 1.5 0 0 0 5 3.5v2a.5.5 0 0 0 1 0v-2z"/>
+                                        <path fill-rule="evenodd" d="M11.854 8.354a.5.5 0 0 0 0-.708l-3-3a.5.5 0 1 0-.708.708L10.293 7.5H1.5a.5.5 0 0 0 0 1h8.793l-2.147 2.146a.5.5 0 0 0 .708.708l3-3z"/>
+                                    </svg>
+                                    {{ $t('Choose options') }}
+                                </router-link>
                             </div>
                         </template>
                         <template v-else>
                             <div class="inner__cart d-flex w-100 bottom-0 mt-3">
-                                <button @click.prevent="showModal = true, picked = { id: item.id, name: translation(item, 'name', $i18n.locale) }" class="btn btn-info btn-sm text-white w-100" type="button">
-                                    <svg xmlns="http://www.w3.org/2000/svg" width="16" height="16" fill="currentColor" class="bi bi-send-plus" viewBox="0 0 16 16">
-                                        <path d="M15.964.686a.5.5 0 0 0-.65-.65L.767 5.855a.75.75 0 0 0-.124 1.329l4.995 3.178 1.531 2.406a.5.5 0 0 0 .844-.536L6.637 10.07l7.494-7.494-1.895 4.738a.5.5 0 1 0 .928.372l2.8-7Zm-2.54 1.183L5.93 9.363 1.591 6.602l11.833-4.733Z"/>
-                                        <path d="M16 12.5a3.5 3.5 0 1 1-7 0 3.5 3.5 0 0 1 7 0Zm-3.5-2a.5.5 0 0 0-.5.5v1h-1a.5.5 0 0 0 0 1h1v1a.5.5 0 0 0 1 0v-1h1a.5.5 0 0 0 0-1h-1v-1a.5.5 0 0 0-.5-.5Z"/>
+                                <button @click.prevent="showModal = true, picked = { id: item.id, name: translation(item, 'name', $i18n.locale) }" class="btn btn-sm btn-link d-flex align-items-center text-dark p-0 text-decoration-none" type="button">
+                                    <svg xmlns="http://www.w3.org/2000/svg" width="16" height="16" fill="currentColor" class="bi bi-envelope me-2" viewBox="0 0 16 16">
+                                        <path d="M0 4a2 2 0 0 1 2-2h12a2 2 0 0 1 2 2v8a2 2 0 0 1-2 2H2a2 2 0 0 1-2-2V4Zm2-1a1 1 0 0 0-1 1v.217l7 4.2 7-4.2V4a1 1 0 0 0-1-1H2Zm13 2.383-4.708 2.825L15 11.105V5.383Zm-.034 6.876-5.64-3.471L8 9.583l-1.326-.795-5.64 3.47A1 1 0 0 0 2 13h12a1 1 0 0 0 .966-.741ZM1 11.105l4.708-2.897L1 5.383v5.722Z"/>
                                     </svg>
                                     {{ $t('Notify') }}
                                 </button>
