399dff0015857c56c06462de6f20c4a70295d113
Chu <cvhainb@gmail.com>
2022-12-07 14:56:01 +0700

    working search form

--- resources/js/components/themes/storefront/templates/layout/Header.vue
+++ resources/js/components/themes/storefront/templates/layout/Header.vue
@@ -12,12 +12,17 @@
                     <display-menu menu-key="primary" :responsive="true"></display-menu>
                 </div>
                 <div class="col-xl-2 col-6 order-xl-3 order-2 navbar-cart-account text-end">
-                    <router-link v-if="isCustomerLogged" to="/account" class="text-white text-decoration-none">
+                    <button @click.stop="openSearchForm" class="btn btn-sm text-white">
+                        <svg xmlns="http://www.w3.org/2000/svg" width="28" height="28" fill="currentColor" class="bi bi-search" viewBox="0 0 16 16">
+                            <path d="M11.742 10.344a6.5 6.5 0 1 0-1.397 1.398h-.001c.03.04.062.078.098.115l3.85 3.85a1 1 0 0 0 1.415-1.414l-3.85-3.85a1.007 1.007 0 0 0-.115-.1zM12 6.5a5.5 5.5 0 1 1-11 0 5.5 5.5 0 0 1 11 0z"/>
+                        </svg>
+                    </button>
+                    <router-link v-if="isCustomerLogged" to="/account" class="text-white text-decoration-none ms-3">
                         <svg xmlns="http://www.w3.org/2000/svg" width="32" height="32" fill="currentColor" class="bi bi-person" viewBox="0 0 16 16">
                             <path d="M8 8a3 3 0 1 0 0-6 3 3 0 0 0 0 6zm2-3a2 2 0 1 1-4 0 2 2 0 0 1 4 0zm4 8c0 1-1 1-1 1H3s-1 0-1-1 1-4 6-4 6 3 6 4zm-1-.004c-.001-.246-.154-.986-.832-1.664C11.516 10.68 10.289 10 8 10c-2.29 0-3.516.68-4.168 1.332-.678.678-.83 1.418-.832 1.664h10z"/>
                         </svg>
                     </router-link>
-                    <router-link v-else to="/register" class="text-white text-decoration-none">
+                    <router-link v-else to="/register" class="text-white text-decoration-none ms-3">
                         <svg xmlns="http://www.w3.org/2000/svg" width="32" height="32" fill="currentColor" class="bi bi-person" viewBox="0 0 16 16">
                             <path d="M8 8a3 3 0 1 0 0-6 3 3 0 0 0 0 6zm2-3a2 2 0 1 1-4 0 2 2 0 0 1 4 0zm4 8c0 1-1 1-1 1H3s-1 0-1-1 1-4 6-4 6 3 6 4zm-1-.004c-.001-.246-.154-.986-.832-1.664C11.516 10.68 10.289 10 8 10c-2.29 0-3.516.68-4.168 1.332-.678.678-.83 1.418-.832 1.664h10z"/>
                         </svg>
@@ -40,13 +45,15 @@
 import Dropdown from 'bootstrap/js/dist/dropdown';
 import BannerSlideshow from '@theme/storefront/templates/banner/Slideshow'
 import DisplayMenu from '@theme/storefront/templates/menu/DisplayMenu'
+import SearchForm from '@theme/storefront/templates/header/SearchForm'
 import { mapState, mapGetters } from 'vuex'
 export default {
     data: () => ({
         selectLanguage: undefined,
-        selectCurrency: undefined
+        selectCurrency: undefined,
+        searchFormStatus: false
     }),
-    components: { BannerSlideshow, DisplayMenu },
+    components: { BannerSlideshow, DisplayMenu, SearchForm },
     mounted() {
         const dropdownElementList = [].slice.call(document.querySelectorAll('.dropdown-toggle'))
         dropdownElementList.map(function (dropdownToggleEl) {
@@ -56,6 +63,9 @@ export default {
     methods: {
         imgloaded(e) {
             return e.target.classList.remove('img-loading')
+        },
+        openSearchForm() {
+            
         }
     },
     computed: {
