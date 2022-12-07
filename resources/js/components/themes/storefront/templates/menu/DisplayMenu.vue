1f40c01858af5c6be448f38f250be1bf926db06f
Chu <cvhainb@gmail.com>
2022-12-07 12:04:43 +0700

    updated

--- resources/js/components/themes/storefront/templates/menu/DisplayMenu.vue
+++ resources/js/components/themes/storefront/templates/menu/DisplayMenu.vue
@@ -22,8 +22,8 @@
     <nav v-if="responsive" :class="`navbar-${menuKey} navbar navbar-expand-lg navbar-light py-0 showing`">
         <div class="container-fluid px-0 position-relative">
             <button class="btn navbar-toggler text-white border position-absolute" type="button" @click.stop="offcanvas.show()">
-                <svg xmlns="http://www.w3.org/2000/svg" width="16" height="16" fill="currentColor" class="bi bi-sliders" viewBox="0 0 16 16">
-                    <path fill-rule="evenodd" d="M11.5 2a1.5 1.5 0 1 0 0 3 1.5 1.5 0 0 0 0-3zM9.05 3a2.5 2.5 0 0 1 4.9 0H16v1h-2.05a2.5 2.5 0 0 1-4.9 0H0V3h9.05zM4.5 7a1.5 1.5 0 1 0 0 3 1.5 1.5 0 0 0 0-3zM2.05 8a2.5 2.5 0 0 1 4.9 0H16v1H6.95a2.5 2.5 0 0 1-4.9 0H0V8h2.05zm9.45 4a1.5 1.5 0 1 0 0 3 1.5 1.5 0 0 0 0-3zm-2.45 1a2.5 2.5 0 0 1 4.9 0H16v1h-2.05a2.5 2.5 0 0 1-4.9 0H0v-1h9.05z"/>
+                <svg xmlns="http://www.w3.org/2000/svg" width="16" height="16" fill="currentColor" class="bi bi-filter-left" viewBox="0 0 16 16">
+                    <path d="M2 10.5a.5.5 0 0 1 .5-.5h3a.5.5 0 0 1 0 1h-3a.5.5 0 0 1-.5-.5zm0-3a.5.5 0 0 1 .5-.5h7a.5.5 0 0 1 0 1h-7a.5.5 0 0 1-.5-.5zm0-3a.5.5 0 0 1 .5-.5h11a.5.5 0 0 1 0 1h-11a.5.5 0 0 1-.5-.5z"/>
                 </svg>
             </button>
             <div class="offcanvas offcanvas-start" tabindex="-1" id="offcanvas-nav">
